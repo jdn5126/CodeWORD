9d139ef6306dbc5cc3575c40a4b6c3c4eae63810
Jeffrey Nelson <jdn5126@gmail.com>
2022-02-27 21:27:53 -0600

    update flask entry point comment

--- CodeWord/__init__.py
+++ CodeWord/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # CodeWord accepts only GET requests, and returns only a single web page and
-# its TypeScript to play the game.
+# its JavaScript dependenies to play the game.
 @app.route('/')
 def index():
     return render_template('index.html')
